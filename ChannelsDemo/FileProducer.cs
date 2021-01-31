e604c3ec9d9729e7eb69feaa381b7fd934de01c9
Alex Vallejo <vallejo.alex@gmail.com>
2021-01-31 14:39:32 -0800

    Initial commit

---
+++ ChannelsDemo/FileProducer.cs
@@ -0,0 +1,10 @@
+﻿namespace ChannelsDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class FileProducer
+  {
+  }
+}
---
+++ ChannelsDemo/Producer.cs
@@ -0,0 +1,28 @@
+﻿namespace ChannelsDemo
+{
+  using System.Threading;
+  using System.Threading.Channels;
+  using System.Threading.Tasks;
+
+  public class Producer : Channel<int>
+  {
+    private readonly CancellationToken token;
+
+    public Producer(CancellationToken token)
+    {
+      this.token = token;
+    }
+
+    public async ValueTask ProduceAsync(int value) => await this.Writer.WriteAsync(value);
+
+    public async Task RunAsync()
+    {
+      await Task.Yield();
+
+      while (!this.token.IsCancellationRequested)
+      {
+        int value = await this.Reader.ReadAsync();
+      }
+    }
+  }
+}
